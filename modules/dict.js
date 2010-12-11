b00645697e70db65d0789023f2eea5456f2e197f
Siddharth Agarwal <sid.bugzilla@gmail.com>
2010-12-11 21:25:47 +0530

    Basic implementation

--- modules/dict.js
+++ modules/dict.js
@@ -35,5 +35,84 @@
  * 
  * ***** END LICENSE BLOCK ***** */
 
+"use strict";
+
 var EXPORTED_SYMBOLS = ["Dict"];
 
+
+function createDictProxy() {
+  // This is inside createDictProxy to be unique for each proxy
+  function DictVal(aVal) {
+    this.value = aVal;
+    // This should be immutable
+    Object.freeze(this);
+  }
+
+  // This is the actual dictionary of items. Each valid item is a DictVal
+  // instance containing the item.
+  let items = {};
+  return {
+    /**
+     * Given a string, returns whether the string is a key in this dictionary.
+     */
+    _isKey: function DictProxy__isKey(aStr) {
+      return (items.hasOwnProperty(aStr) && items[aStr] instanceof DictVal);
+    },
+
+    getOwnPropertyDescriptor: function DictProxy_getOwnPropertyDescriptor(aName) {
+      if (!this._isKey(aName))
+        return undefined;
+
+      return {
+        value: items[aName].value,
+        writable: true,
+        configurable: true,
+        enumerable: true,
+      };
+    },
+
+    getOwnPropertyNames: function DictProxy_getOwnPropertyNames(aName) {
+      // getOwnPropertyNames on items should return exactly the set of
+      // keys that we've added.
+      return Object.getOwnPropertyNames(items);
+    },
+
+    delete: function DictProxy_delete(aName) {
+      if (!this._isKey(aName))
+        return (delete items[aName]);
+      return false;
+    },
+
+    has: function DictProxy_has(aName) {
+      // We make "in" behave like hasOwnProperty
+      return this._isKey(aName);
+    },
+
+    hasOwn: function DictProxy_hasOwn(aName) {
+      return this._isKey(aName);
+    },
+
+    get: function DictProxy_get(aReceiver, aName) {
+      if (this._isKey(aName))
+        return items[aName].value;
+      return items[aName];
+    },
+
+    set: function DictProxy_set(aReceiver, aName, aVal) {
+      items[aName] = new DictVal(aVal);
+    },
+
+    enumerate: function DictProxy_enumerate() {
+      // We make for..in behave like Object.keys
+      return Object.keys(items);
+    },
+
+    keys: function DictProxy_keys() {
+      return Object.keys(items);
+    },
+  };
+}
+
+function Dict() {
+  this.__proto__ = createDictProxy();
+}
